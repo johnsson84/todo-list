2da6f346a37b1d27947f9cedad0eb81ecc1089e2
Johan Johnsson <johan@johnsson-net.se>
2023-09-13 18:15:01 +0200

    added menu 1 and 3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +6,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("TODO LIST");
         boolean isRunning = true;
+        ArrayList<String> list = new ArrayList<>();
         while (isRunning) {
             System.out.println("1. Add item" +
                     "\n2. Delete item" +
@@ -12,14 +14,30 @@ public class Main {
                     "\n4. Exit program");
             System.out.print("Enter: ");
             String choice = input.nextLine();
+            System.out.println();
             switch (choice) {
                 case "1": {
+                    System.out.println("Add something to the list: ");
+                    String a = input.nextLine();
+                    list.add(a);
+                    System.out.println(a + " was added to the list!");
+                    System.out.println();
                     break;
                 }
                 case "2": {
                     break;
                 }
                 case "3": {
+                    if (list.isEmpty()) {
+                        System.out.println("\nList is empty...");
+                    }
+                    else {
+                        System.out.println("\nList:");
+                        for (int i = 0; i < list.size(); i++) {
+                            System.out.println((i+1) + ". " + list.get(i));
+                        }
+                    }
+                    System.out.println();
                     break;
                 }
                 case "4": {
